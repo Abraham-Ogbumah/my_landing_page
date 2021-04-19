1cb2d55c28ddb0a80391914a9784f766c7ba5abf
Abraham-Ogbumah <ab.ogbumah@gmail.com>
2021-04-19 23:29:36 +0100

    Updates
    Updates to the JS file with the add active class function, the CSS with the important key word for the active section and the ReadMe with relevant information

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # my_landing_page
  Creating a landing page with HTML, CSS and Javascript functionalities
+
+
+# Description
+The project uses javaScript to create a dynamic navigation bar. The items of the navigation are named using the section id's of page. When a section is deleted from the page, the corresponding menu is taken out and vice versa.
+
+The project also implements smooth scroll using javaScript. Though a CSS rule could have been used to acheive this.
+
+Finally the project add the active class to the current section been viewed on the page.
+
+# Prerequisite
+Just a browser to be able to access the page
+
+# Installation
+Just your code editor to view the raw code and a browser to see it live.
+
+# Runtime Environment
+
+Chrome
--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,18 @@
 const body = document.body;
-const navLinks = [...document.querySelectorAll('section')].map(item => item.id); //Extract the Section names as a nodeList
-const linksList = document.querySelector('.items');
-const sectionList = document.querySelectorAll('section');
-//const classElement = document.querySelector('.active');
+const navLinks = [...document.querySelectorAll('section')].map(item => item.id); //Extract the Section names as a nodeList, convert to an array and store is navLinks
+const linksList = document.querySelector('.items'); //get the element with a class selector of items and assign to linksList
+const sectionList = document.querySelectorAll('section'); //Get all sections and store in a nodelist
 
+
+/*******************************************************
+The function below build a dynamic nav bar using the section
+ids by creating the li and a elements and appends both the
+the unordered list The class and data-sections are also
+assigned to the a elements once created
+
+The nav bar is built such that when new sections are created,
+they are automatically added to the navigation items
+********************************************************/
 function buildNav() {
     for (const link of navLinks) {
         let a = document.createElement("a");
@@ -12,6 +21,7 @@ function buildNav() {
         a.href = '#' + link;
         a.innerText = link;
         a.setAttribute('class', 'links');
+        a.setAttribute('data-section', a.innerText);
 
         li.appendChild(a);
         linksList.appendChild(li);
@@ -20,10 +30,8 @@ function buildNav() {
 
 buildNav();
 
-//window.addEventListener("load", buildNav);
 
-
-//The section below implements the smooth scroll behavior
+//The function below implements the smooth scroll behavior
 const anchor = document.querySelectorAll('.links');
 anchor.forEach(a => {
     a.addEventListener('click', function(evt) {
@@ -34,3 +42,19 @@ anchor.forEach(a => {
         });
     })
 });
+
+// Add class 'active' to section when near top of viewport
+function addActiveClass() {
+    for (let section of sectionList) {
+        const item = section.getBoundingClientRect();
+        const activeElement = document.getElementById(section.id);
+        if (item.y <= 90 && item.bottom >= 100) {
+            section.classList.add("active");
+        } else {
+            section.classList.remove("active");
+        }
+    }
+}
+
+//the event listener listens for a scroll event and call the corresponding section to add the active class
+document.addEventListener('scroll', addActiveClass);
